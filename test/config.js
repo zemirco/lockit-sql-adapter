9f8dce57cab06ec68e3c2ce44824c9dd6887d862
zeMirco <mirco.zeiss@gmail.com>
2014-01-20 21:11:57 +0100

    run travis tests for all supported dbs

--- test/config.js
+++ test/config.js
@@ -1,3 +1,7 @@
-exports.db = 'postgresql';
-exports.dbUrl = 'postgres://127.0.0.1:5432/users';
+exports.db = 'sql';
+exports.dbUrls = {
+  postgres: 'postgres://127.0.0.1:5432/users',
+  mysql: 'mysql://127.0.0.1:8889/users',
+  sqlite: 'sqlite://:memory:'
+};
 exports.dbCollection = 'my_user_table';
--- test/test.js
+++ test/test.js
@@ -1,127 +1,137 @@
 
 var should = require('should');
 var config = require('./config.js');
-var adapter = require('../index.js')(config);
 
-// start the tests
-describe('sql adapter for lockit', function() {
+// run the same tests for postgres, mysql and sqlite
+Object.keys(config.dbUrls).forEach(function(obj) {
+  
+  var conf = {
+    dbUrl: config.dbUrls[obj],
+    dbCollection: config.dbCollection
+  };
 
-  // needed later for test 'find user by signup token'
-  var _tmp_signupToken = '';
+  var adapter = require('../index.js')(conf);
 
-  it('should create a new user', function(done) {
-
-    adapter.save('john', 'john@email.com', 'secret', function(err, res) {
-      if (err) console.log(err);
-
-      res.should.have.property('signupToken');
-      res.signupToken.should.match(/[0-9a-f]{22}|[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/);
-      res.should.have.property('failedLoginAttempts');
-      res.failedLoginAttempts.should.equal(0);
-      _tmp_signupToken = res.signupToken;
-      res.email.should.equal('john@email.com');
-      done();
-
-    });
-
-  });
-
-  it('should find a user by username', function(done) {
-
-    adapter.find('username', 'john', function(err, res) {
-      if (err) console.log(err);
-      res.username.should.equal('john');
-      res.email.should.equal('john@email.com');
-      done();
-    });
-
-  });
-
-  it('should return undefined when no user is found', function(done) {
-
-    adapter.find('username', 'jim', function(err, res) {
-      if (err) console.log(err);
-
-      should.not.exist(err);
-      should.not.exist(res);
-      done();
-    });
-
-  });
-
-  it('should find a user by email', function(done) {
-
-    adapter.find('email', 'john@email.com', function(err, res) {
-      if (err) console.log(err);
-      res.username.should.equal('john');
-      res.email.should.equal('john@email.com');
-      done();
-    });
-
-  });
-
-  it('should find a user by signup token', function(done) {
-
-    adapter.find('signupToken', _tmp_signupToken, function(err, res) {
-      if (err) console.log(err);
-      res.username.should.equal('john');
-      res.email.should.equal('john@email.com');
-      done();
-    });
-
-  });
-
-  it('should update an existing user', function(done) {
-
-    adapter.find('username', 'john', function(err, doc) {
-      if (err) console.log(err);
-
-      doc.signupToken = 'works';
-      doc.accountLocked = true;
-
-      adapter.update(doc, function(err, res) {
+  // start the tests
+  describe(obj + ' adapter for lockit', function() {
+  
+    // needed later for test 'find user by signup token'
+    var _tmp_signupToken = '';
+  
+    it('should create a new user', function(done) {
+  
+      adapter.save('john', 'john@email.com', 'secret', function(err, res) {
         if (err) console.log(err);
-
-        res.signupToken.should.equal('works');
-        res.accountLocked.should.be.true;
+  
+        res.should.have.property('signupToken');
+        res.signupToken.should.match(/[0-9a-f]{22}|[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/);
+        res.should.have.property('failedLoginAttempts');
+        res.failedLoginAttempts.should.equal(0);
+        _tmp_signupToken = res.signupToken;
+        res.email.should.equal('john@email.com');
         done();
-
+  
       });
-
+  
     });
-
-  });
-
-  it('should remove a user', function(done) {
-
-    var user = {
-      username: 'jeff',
-      email: 'jeff@email.com'
-    };
-
-    adapter.save('jeff', 'jeff@email.com', 'secret', function(err, res) {
-      if (err) console.log(err);
-      adapter.remove('username', 'jeff', function(err, res) {
+  
+    it('should find a user by username', function(done) {
+  
+      adapter.find('username', 'john', function(err, res) {
         if (err) console.log(err);
-        res.should.be.true;
+        res.username.should.equal('john');
+        res.email.should.equal('john@email.com');
         done();
       });
+  
     });
-
-  });
-
-  it('should return an error when remove cannot find a user', function(done) {
-    adapter.remove('username', 'steve', function(err, res) {
-      err.message.should.equal('lockit - Cannot find username: "steve"');
-      done();
+  
+    it('should return undefined when no user is found', function(done) {
+  
+      adapter.find('username', 'jim', function(err, res) {
+        if (err) console.log(err);
+  
+        should.not.exist(err);
+        should.not.exist(res);
+        done();
+      });
+  
     });
+  
+    it('should find a user by email', function(done) {
+  
+      adapter.find('email', 'john@email.com', function(err, res) {
+        if (err) console.log(err);
+        res.username.should.equal('john');
+        res.email.should.equal('john@email.com');
+        done();
+      });
+  
+    });
+  
+    it('should find a user by signup token', function(done) {
+  
+      adapter.find('signupToken', _tmp_signupToken, function(err, res) {
+        if (err) console.log(err);
+        res.username.should.equal('john');
+        res.email.should.equal('john@email.com');
+        done();
+      });
+  
+    });
+  
+    it('should update an existing user', function(done) {
+  
+      adapter.find('username', 'john', function(err, doc) {
+        if (err) console.log(err);
+  
+        doc.signupToken = 'works';
+        doc.accountLocked = true;
+  
+        adapter.update(doc, function(err, res) {
+          if (err) console.log(err);
+  
+          res.signupToken.should.equal('works');
+          res.accountLocked.should.be.true;
+          done();
+  
+        });
+  
+      });
+  
+    });
+  
+    it('should remove a user', function(done) {
+  
+      var user = {
+        username: 'jeff',
+        email: 'jeff@email.com'
+      };
+  
+      adapter.save('jeff', 'jeff@email.com', 'secret', function(err, res) {
+        if (err) console.log(err);
+        adapter.remove('username', 'jeff', function(err, res) {
+          if (err) console.log(err);
+          res.should.be.true;
+          done();
+        });
+      });
+  
+    });
+  
+    it('should return an error when remove cannot find a user', function(done) {
+      adapter.remove('username', 'steve', function(err, res) {
+        err.message.should.equal('lockit - Cannot find username: "steve"');
+        done();
+      });
+    });
+  
   });
-
-});
-
-// remove users db
-after(function(done) {
-
-  adapter.remove('username', 'john', done);
-
+  
+  // remove users db
+  after(function(done) {
+  
+    adapter.remove('username', 'john', done);
+  
+  });
 });
