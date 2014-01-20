ca2ce5f69dc1b5733960d25aff8af38002528e54
zeMirco <mirco.zeiss@gmail.com>
2014-01-20 21:32:49 +0100

    use username travis for mysql

--- test/config.js
+++ test/config.js
@@ -1,7 +1,7 @@
 exports.db = 'sql';
 exports.dbUrls = {
 //  postgres: 'postgres://127.0.0.1:5432/users',
-  mysql: 'mysql://127.0.0.1:3306/users',
+  mysql: 'mysql://travis:@127.0.0.1:3306/users',
   sqlite: 'sqlite://:memory:'
 };
 exports.dbCollection = 'my_user_table';
