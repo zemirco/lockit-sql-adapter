c3c510d1cdf4c9b6547db26cdd834c6661815daa
zeMirco <mirco.zeiss@gmail.com>
2014-04-17 21:17:11 +0200

    update config and use object for db connection

--- test/config.js
+++ test/config.js
@@ -1,9 +1,20 @@
 exports.dbs = {
-  postgres: 'postgres://postgres:@127.0.0.1:5432/users',
-  mysql: 'mysql://travis:@127.0.0.1:3306/users',
-  sqlite: 'sqlite://:memory:'
+  postgres: {
+    url: 'postgres://postgres:@127.0.0.1:5432/',
+    name: 'users',
+    collection: 'my_user_table'
+  },
+  mysql: {
+    url: 'mysql://travis:@127.0.0.1:3306/',
+    name: 'users',
+    collection: 'my_user_table'
+  },
+  sqlite: {
+    url: 'sqlite://',
+    name: ':memory:',
+    collection: 'my_user_table'
+  }
 };
-exports.dbCollection = 'my_user_table';
 
 exports.signup = {
   tokenExpiration: '1 day'
